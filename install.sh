c506f0374480de06a124351312a92380a0c97042
Ruan Silva <progmruansilva@gmail.com>
2025-09-14 22:07:58 -0300

    ✨ (feat): Add atpr to PATH

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
 
-# This script installs the 'pull_request' pattern into the fabric patterns directory.
-
-# Define the source directory for the pattern.
-# It's the 'sample/pull_request' directory relative to this script's location.
 SOURCE_DIR="./sample/pull_request"
 
-# Define the destination directory for fabric patterns.
 DEST_DIR="$HOME/.config/fabric/patterns/"
 
-# Create the destination directory if it doesn't exist.
 echo "Creating pattern directory: $DEST_DIR"
 mkdir -p "$DEST_DIR"
 
-# Copy the pattern files from the source to the destination.
 echo "Installing 'pull_request' pattern..."
-cp -R "$SOURCE_DIR"/* "$DEST_DIR/"
+cp -R "$SOURCE_DIR" "$DEST_DIR/"
 
 echo "✅ Installation successful."
 echo "The 'pull_request' pattern is now available in fabric."
+
+
+echo "Installing 'atpr' command..."
+chmod +x atpr.sh
+sudo cp -r atpr.sh /usr/local/bin/atpr
+
+echo "✅ 'atpr' command is now available"
